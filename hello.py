d2ce3542cbe165a08231218b7c41320df232e1aa
Vivekkumar0412 <www.vivekkumar0412@gmail.com>
2023-08-25 17:17:02 +0530

    basic programs in python

--- hello.py
+++ hello.py
@@ -1,1 +1,52 @@
-print("Vivek is here")
+# print("Vivek is here")
+# a = 10
+# b = 10
+# print("The sum of a and b is : ", a + b)
+# print(id(a),id(b))
+# c = "Vivek"
+# print(c)
+#
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a%b)
+# print(a/b)
+# print(a**2) #10 to the power
+# print(a//b) # floor division means it will neflect the decimal value only integer part will be printed
+# a+=59
+# print(a)
+a = 10
+b = 10
+# b = "11111"
+print(b)
+str1 = "vivek"
+print('wwk' in str1)
+print(type(b))
+print(5 + (2*b))
+a = 2 + 5j
+
+v = [1,2,3,4,"vivek","singh"]
+print(13 in v)
+print(v[4])
+print("iv" in v[4])
+
+vk = (1,2,3,4,"vuic","989")
+d = {
+    "first" : 1,
+    "second" : 2,
+    "name" : "vivek singh rajput"
+}
+print(vk[5])
+t = (10)
+print(t, type(t))
+d1 = {
+    "first" : 1,
+    "second" : 2,
+    "name" : "vivek singh rajput"
+}
+print(d1, type(d1))
+# print(d.second)
+d1["name"] = "vivek"
+print(d1["name"])
+s = {1,1,2,2,3,3,4,4,}
+print(s)
