340d94e62bc87fc19a5c6ffb8ab4cad2ecfccdc5
Vivekkumar0412 <www.vivekkumar0412@gmail.com>
2023-08-28 03:35:21 +0530

    many important concepts of python

--- hello.py
+++ hello.py
@@ -1,55 +1,131 @@
-# print("Vivek is here")
+# # print("Vivek is here")
+# # a = 10
+# # b = 10
+# # print("The sum of a and b is : ", a + b)
+# # print(id(a),id(b))
+# # c = "Vivek"
+# # print(c)
+# #
+# # print(a+b)
+# # print(a-b)
+# # print(a*b)
+# # print(a%b)
+# # print(a/b)
+# # print(a**2) #10 to the power
+# # print(a//b) # floor division means it will neflect the decimal value only integer part will be printed
+# # a+=59
+# # print(a)
 # a = 10
 # b = 10
-# print("The sum of a and b is : ", a + b)
-# print(id(a),id(b))
-# c = "Vivek"
-# print(c)
+# # b = "11111"
+# print(b)
+# str1 = "vivek"
+# print('wwk' in str1)
+# print(type(b))
+# print(5 + (2*b))
+# a = 2 + 5j
 #
-# print(a+b)
-# print(a-b)
-# print(a*b)
-# print(a%b)
-# print(a/b)
-# print(a**2) #10 to the power
-# print(a//b) # floor division means it will neflect the decimal value only integer part will be printed
-# a+=59
-# print(a)
-a = 10
-b = 10
-# b = "11111"
-print(b)
-str1 = "vivek"
-print('wwk' in str1)
-print(type(b))
-print(5 + (2*b))
-a = 2 + 5j
+# v = [1,2,3,4,"vivek","singh"]
+# print(13 in v)
+# print(v[4])
+# print("iv" in v[4])
+#
+# vk = (1,2,3,4,"vuic","989")
+# d = {
+#     "first" : 1,
+#     "second" : 2,
+#     "name" : "vivek singh rajput"
+# }
+# print(vk[5])
+# t = (10)
+# print(t, type(t))
+# d1 = {
+#     "first" : 1,
+#     "second" : 2,
+#     "name" : "vivek singh rajput"
+# }
+# print(d1, type(d1))
+# # print(d.second)
+# d1["name"] = "vivek"
+# print(d1["name"])
+# s = {1,1,2,2,3,3,4,4,}
+# print(s)
+# print(s,"s is here", type(s))
+#
+# print("New day new code")
+strr = "Vivek"
+# this is an error we can not do this thing we can assing anything multiple timees but we can not update it.
+strr = "vicek kr"
+# # strr[0] = "z"
+# a = (1,1,1,1,)
+# # print(a[0])
+# # print(strr[0])
+# a = int(input("Enter : -"))
+# b = int(a)
+# print(a,"after ", type(a))
+# print(b, type(b))
+#
+# c = eval(input("enyter c"))
+# # d = eval(input("enter d"))
+# # e = eval(input("endter d"))
+# # print(c + d + e)
+# if c % 2 == 0:
+#     print("even hai bhai")
+# else:
+#     print("odd hai bhai")
+#
+# d = eval(input("Enter the marks"))
+# if d >= 75:
+#     print("GREAT")
+# elif d >= 60:
+#     print(("V.GOOD"))
+# elif d >= 50:
+#     print("good need improvement")
+# else:
+#     print(("FAIL"))
 
-v = [1,2,3,4,"vivek","singh"]
-print(13 in v)
-print(v[4])
-print("iv" in v[4])
+# for v in range(0,6,2):
+#     print(v)
+# for b in range(1,11):
+#     print("2 x ",b," = ", 2*b)
+#
+for j in range(10,0,-1):
+    print(j)
 
-vk = (1,2,3,4,"vuic","989")
-d = {
-    "first" : 1,
-    "second" : 2,
-    "name" : "vivek singh rajput"
-}
-print(vk[5])
-t = (10)
-print(t, type(t))
-d1 = {
-    "first" : 1,
-    "second" : 2,
-    "name" : "vivek singh rajput"
-}
-print(d1, type(d1))
-# print(d.second)
-d1["name"] = "vivek"
-print(d1["name"])
-s = {1,1,2,2,3,3,4,4,}
-print(s)
-print(s,"s is here", type(s))
+i = 10
+while i > 0:
+    print(i, "Vivek")
+    i -= 1
 
-print("New day new code")
+s = "vivek"
+# print("i" in strr)
+print(s[0:])
+
+print(s[-1::-1])
+st = "nitin jha hai naam"
+stt = s[-1::-1]
+print(stt , " stt is here")
+if st == stt:
+    print("Palindrome")
+else:
+    print("not a palindorm")
+
+# for i in range(len(st)):
+#     for j in range(len(stt)):
+#         if st[i] == stt[j]:
+#             print("Palindrome")
+#         else:
+#             print("Palindrome not hshehee")
+print(st.upper())
+print(st.lower())
+print(st.title())
+print(st.capitalize())
+print(st.index("jha"))
+print(st.lower().find("Jha"))
+
+jk = "123 hlhhi"
+print(jk.isalnum())
+print(chr(65))
+
+v = "VIVEK {} RAJPUT".format("SINGH")
+print(v)
