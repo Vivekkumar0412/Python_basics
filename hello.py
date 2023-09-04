99f9a8c0a860eb53124abda6347f7b93c4244b08
Vivekkumar0412 <www.vivekkumar0412@gmail.com>
2023-09-04 11:39:40 +0530

    star pattern in python

--- hello.py
+++ hello.py
@@ -330,3 +330,11 @@ for i in range(1,6):
         else:
             print(" ")
     print()
+
+for i in range(1,6):
+    for j in range(1,6):
+        if (i == 1 or i == n) or (j == 1 or j == m):
+            print("*",end="")
+        else:
+            print(" ")
+    print()
